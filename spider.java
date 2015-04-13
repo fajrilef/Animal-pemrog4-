7cc8323615a6637063274950907b3d0a76489a6a
Fajri Adha <fajrilef@yahoo.com>
2015-04-13 21:47:11 +0700

    Create spider.java

---
+++ spider.java
@@ -0,0 +1,18 @@
+package animal;
+
+/**
+ *
+ * @author fajri
+ */
+public class spider extends Animal{
+    public spider(){
+        
+    }
+    public void spider(){
+        System.out.println("laba-laba memiliki sarang");
+    }
+    
+    public void eat(){
+        System.out.println("laba-laba dapat makan");
+    }
+}
